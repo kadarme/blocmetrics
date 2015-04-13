7daf54d0675477e01f3911f37678fb32401d9559
kadarme <kn.adarme@gmail.com>
2015-04-13 22:47:59 +0000

    Worked on api controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :registered_applications
 
+  namespace :api, defaults: { format: :json } do
+    resources :events, only: [:create]
+  end
+  
   devise_for :users
   get 'about' => 'welcome#about'
 
